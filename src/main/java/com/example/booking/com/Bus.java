89cba3bb4e05f6cc99bf2d9954b7b9ad01d9c7e5
Shubham <shubhamveer111@gmail.com>
2023-01-30 19:08:21 +0530

    New-File-Add

---
+++ src/main/java/com/example/booking/com/Bus.java
@@ -0,0 +1,12 @@
+package com.example.booking.com;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class Bus {
+    @GetMapping("/MYBUSBOOKING")
+    public String getData() {
+        return "Please book your bus ticket";
+    }
+}
+
